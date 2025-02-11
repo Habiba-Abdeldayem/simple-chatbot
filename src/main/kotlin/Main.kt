291b896fcf4a2127528202e5cd012ffb4c7a980b
Habiba <habiba.abdeldayem.dev@gmail.com>
2025-02-11 07:17:52 +0200

    enhancement: improve Scanner handling with .use

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,14 +4,16 @@ import java.time.Year
 import java.util.Scanner
 
 fun main() {
-    val scanner = Scanner(System.`in`)
+
     val botName = "Aid"
     val birthYear = Year.now().value
     println("Hello! My name is $botName.")
     println("I was created in $birthYear.")
     println("Please, remind me your name.")
 
-    val userName = scanner.next()
-    println("What a great name you have, $userName!")
+    Scanner(System.`in`).use { scanner ->
+        val userName = scanner.next()
+        println("What a great name you have, $userName!")
+    } // Scanner automatically closes here ✅
 
 }
