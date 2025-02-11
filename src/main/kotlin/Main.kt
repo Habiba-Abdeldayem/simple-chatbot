96527b9f9ecc744118ad9c2b9b3db2191476aa42
Habiba <habiba.abdeldayem.dev@gmail.com>
2025-02-11 10:10:15 +0200

    Refactored counting logic to use repeat() instead of for-loop for simplicity

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,11 +6,10 @@ fun main() {
 
     val botName = "Aid"
     val birthYear = Year.now().value
-
     println("Hello! My name is $botName.")
     println("I was created in $birthYear.")
-    println("Please, remind me your name.")
 
+    println("Please, remind me your name.")
     val userName = readln()
     println("What a great name you have, $userName!")
 
@@ -22,11 +21,10 @@ fun main() {
     val remainder5 = readln().toInt()
     val remainder7 = readln().toInt()
     val age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
-
     println("Your age is $age; that's a good time to start programming!")
 
     println("Now I will prove to you that I can count to any number you want.")
     val number = readln().toInt()
-    for (i in 0..number) println("$i!")
+    repeat(number + 1) { println("$it!") }
     println("Completed, have a nice day!")
 }
