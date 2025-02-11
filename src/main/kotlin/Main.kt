e5bf2bfe61923fb76414d150e0a6f8f23000234e
Habiba <habiba.abdeldayem.dev@gmail.com>
2025-02-11 07:37:35 +0200

    feat: implement age guessing feature

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,28 @@
 package org.example
 
 import java.time.Year
-import java.util.Scanner
 
 fun main() {
 
     val botName = "Aid"
     val birthYear = Year.now().value
+
     println("Hello! My name is $botName.")
     println("I was created in $birthYear.")
     println("Please, remind me your name.")
 
-    Scanner(System.`in`).use { scanner ->
-        val userName = scanner.next()
-        println("What a great name you have, $userName!")
-    } // Scanner automatically closes here ✅
+    val userName = readln()
+    println("What a great name you have, $userName!")
+
+    println("Let me guess your age.")
+    println("Enter remainders of dividing your age by 3, 5 and 7.")
+
+    // reading all remainders
+    val remainder3 = readln().toInt()
+    val remainder5 = readln().toInt()
+    val remainder7 = readln().toInt()
+    val age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+
+    println("Your age is $age; that's a good time to start programming!")
 
 }
