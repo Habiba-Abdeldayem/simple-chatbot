d28d6f9e64167c7b9d55d8fa61701e6137913a3d
Habiba <habiba.abdeldayem.dev@gmail.com>
2025-02-11 07:10:37 +0200

    feat(chatbot): greet user by name

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,17 @@
 package org.example
 
 import java.time.Year
+import java.util.Scanner
 
 fun main() {
+    val scanner = Scanner(System.`in`)
     val botName = "Aid"
     val birthYear = Year.now().value
     println("Hello! My name is $botName.")
     println("I was created in $birthYear.")
+    println("Please, remind me your name.")
+
+    val userName = scanner.next()
+    println("What a great name you have, $userName!")
+
 }
